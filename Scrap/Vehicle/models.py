3ee9fd13b75f73c68f3f6556f206c9ec0ac8a79f
abdullah atiq <abdullah.s.alatiq@gmail.com>
2024-12-10 15:27:46 +0300

    done from brand and car

--- Scrap/Vehicle/models.py
+++ Scrap/Vehicle/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=250, unique=True)  
+    logo = models.ImageField(upload_to='brand_logos/')
+
+    def __str__(self):
+        return self.name
+    
+class Car(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    name = models.CharField(max_length=250, unique=True) 
+
+    def __str__(self):
+        return f"{self.name}"
--- Scrap/Vehicle/urls.py
+++ Scrap/Vehicle/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = "Vehicle"
 urlpatterns = [
     path("brand/", views.new_brand, name="new_brand"),
+    path("car/", views.new_car, name="new_car"),
+
 ]
