827bf16e9559c7784e6de7f50dce50c6a9d283e9
aarokan <aarokan@gmail.com>
2024-12-12 14:08:48 +0300

    create parts list to include

--- Scrap/autoparts/urls.py
+++ Scrap/autoparts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "main"
+app_name = "autoparts"
 
 urlpatterns = [
     path("all_parts/", views.all_parts_view, name="all_parts_view"),
-    path("products/", views.products_view, name="products_view"),
+    path("products/<product_id>/", views.products_view, name="products_view"),
     path("save_categories_to_database/", views.save_categories_to_database, name="save_categories_to_database"),
     path("save_parts_to_database/", views.save_parts_to_database, name="save_parts_to_database"),
 ]
--- Scrap/autoparts/views.py
+++ Scrap/autoparts/views.py
@@ -7,7 +7,7 @@ from .models import Category, Part, Product
 def all_parts_view(request: HttpRequest):
     categories = Category.objects.all()
     parts = Part.objects.all()
-    context = {"categories": categories, "parts": parts}
+    context = {"categories": categories}
 
     return render(request, "autoparts/all_parts.html", context)
 
@@ -55,29 +55,33 @@ def save_categories_to_database(request: HttpRequest):
 def save_parts_to_database(request: HttpRequest):
     # Add parts to the database
     parts_data = [
-        {"part_category_id": 2, "image": "starter.jpg", "name": "سلف"},
-        {"part_category_id": 3, "image": "bumber_front.jpg", "name": "صدام أمامي"},
-        {"part_category_id": 1, "image": "engine.jpg", "name": "المكينة"},
-        {"part_category_id": 7, "image": "lower_control.jpg", "name": "مقصات"},
-        {"part_category_id": 3, "image": "tail_light.jpg", "name": "سطب خلفي"},
-        {"part_category_id": 8, "image": "abs.jpg", "name": "جهاز ABS"},
-        {"part_category_id": 3, "image": "bumber_rear.jpg", "name": "صدام خلفي"},
-        {"part_category_id": 3, "image": "door_back.jpg", "name": "باب خلفي"},
-        {"part_category_id": 8, "image": "frame.jpg", "name": "شاصية المكينة"},
-        {"part_category_id": 6, "image": "seat_front.jpg", "name": "مرتبة أمامية"},
-        {"part_category_id": 1, "image": "transfer_case.jpg", "name": "دبل"},
-        {"part_category_id": 2, "image": "alternator.jpg", "name": "دينامو"},
-        {"part_category_id": 5, "image": "compressor.jpg", "name": "كمبروسر"},
-        {"part_category_id": 3, "image": "door_front.jpg", "name": "باب أمامي"},
-        {"part_category_id": 4, "image": "headlight.jpg", "name": "شمعات"},
-        {"part_category_id": 6, "image": "seat_rear.jpg", "name": "مرتبة خلفية"},
-        {"part_category_id": 1, "image": "transmission.jpg", "name": "القير"},
-        {"part_category_id": 7, "image": "axle_shaft.jpg", "name": "عكس"},
-        {"part_category_id": 5, "image": "cooling_fan.jpg", "name": "مراوح تبريد"},
-        {"part_category_id": 8, "image": "drive_shaft.jpg", "name": "عمود دبل"},
-        {"part_category_id": 2, "image": "ignition_switch.jpg", "name": "سويتش"},
-        {"part_category_id": 6, "image": "speedometer.jpg", "name": "عداد طبلون"},
-        {"part_category_id": 2, "image": "wiper_motor.jpg", "name": "دينمو مساحات"},
+        {"part_category_id": 1, "image": "engine.jpg", "name": "مكينة", "alternative_name": "مكاين مكينة مكنة مكينه مكنه"},
+        {"part_category_id": 1, "image": "transfer_case.jpg", "name": "دبل", "alternative_name": "الدبل دبل"},
+        {"part_category_id": 1, "image": "transmission.jpg", "name": "قير", "alternative_name": "قيربوكس جير جيربوكس قربوكس جربوكس القير القيربوكس الجربوكس الجيربوكس"},
+        {"part_category_id": 2, "image": "starter.jpg", "name": "سلف", "alternative_name": "سلف مرش السلف المرش"},
+        {"part_category_id": 2, "image": "wiper_motor.jpg", "name": "دينمو مساحات", "alternative_name": "دنمو مساحات مكينة مساحات مكينه مساحات دينمو مساحه دينمو مساحة"},
+        {"part_category_id": 2, "image": "ignition_switch.jpg", "name": "سويتش", "alternative_name": "سويتش سوتش دقمه دقمة سستم تشغيل سيستم تشغيل مفتاح ايموبلايزر"},
+        {"part_category_id": 2, "image": "alternator.jpg", "name": "دينامو", "alternative_name": "دينمو دنمو دينامو شحن دنمو شحن داينمو تعبئة"},
+        {"part_category_id": 3, "image": "bumber_front.jpg", "name": "صدام أمامي", "alternative_name": "صدام امامي صدم أمامي"},
+        {"part_category_id": 3, "image": "bumber_rear.jpg", "name": "صدام خلفي", "alternative_name": "صدم خلفي صدام"},
+        {"part_category_id": 3, "image": "door_back.jpg", "name": "باب خلفي", "alternative_name": "بيبان خلفية أبواب خلفيه ابواب خلفية"},
+        {"part_category_id": 3, "image": "door_front.jpg", "name": "باب أمامي", "alternative_name": "بيبان امامي ابواب امامي"},
+        {"part_category_id": 4, "image": "headlight.jpg", "name": "شمعة", "alternative_name": "شمعات شمعه"},
+        {"part_category_id": 4, "image": "tail_light.jpg", "name": "سطب خلفي", "alternative_name": "اسطب سطبات خلفيه سطبات خلفية"},
+        {"part_category_id": 4, "image": "fog_lights.jpg", "name": "كشاف", "alternative_name": "كشافات كشاف ضباب كشاف تحت"},
+        {"part_category_id": 5, "image": "compressor.jpg", "name": "كمبروسر", "alternative_name": "كومبرسور كمبروسور كمبرسر"},
+        {"part_category_id": 5, "image": "radiator.jpg", "name": "راديتر", "alternative_name": "رديتر رادتر رديترات رديترات"},
+        {"part_category_id": 5, "image": "cooling_fan.jpg", "name": "مراوح تبريد", "alternative_name": "مروحة مروحه "},
+        {"part_category_id": 6, "image": "seat_rear.jpg", "name": "مرتبة خلفية", "alternative_name": "مرتبه مراتب خلفية مراتب خلفيه مرتبه ثانية مرتبة ثانية"},
+        {"part_category_id": 6, "image": "seat_front.jpg", "name": "مرتبة أمامية", "alternative_name": "مرتبه امامية مرتبة اماميه"},
+        {"part_category_id": 6, "image": "speedometer.jpg", "name": "عداد طبلون", "alternative_name": ""},
+        {"part_category_id": 7, "image": "lower_control.jpg", "name": "مقصات", "alternative_name": "مقص"},
+        {"part_category_id": 7, "image": "axle_shaft.jpg", "name": "عكس", "alternative_name": "عكوس"},
+        {"part_category_id": 7, "image": "shock_absorber.jpg", "name": "مساعد", "alternative_name": "مساعدات"},
+        {"part_category_id": 8, "image": "abs.jpg", "name": "جهاز ABS", "alternative_name": ""},
+        {"part_category_id": 8, "image": "frame.jpg", "name": "شاصية المكينة", "alternative_name": "شاصيه مكينه"},
+        {"part_category_id": 8, "image": "drive_shaft.jpg", "name": "عمود دبل", "alternative_name": ""},
+        {"part_category_id": 9, "image": "default.svg", "name": "كونسول", "alternative_name": ""},
     ]
 
     parts_to_save = []
