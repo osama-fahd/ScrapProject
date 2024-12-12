64f88ac4a2fdaf4189ad3a264b8b6b442da3fdbc
SalehBinHamed <Saleh.Alrashidi.H@gmail.com>
2024-12-12 19:50:06 +0300

    add product views alomst working

--- Scrap/sellers/views.py
+++ Scrap/sellers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from accounts.models import ProfileSeller
 from autoparts.models import Product, Part
 from Vehicle.models import Brand , Car
+# from sellers.forms import ProductForm
 
 # Create your views here.
 
@@ -15,7 +16,39 @@ def seller_products(request):
 def seller_add_product(request):
     cars = Car.objects.all()
     parts = Part.objects.all()
-    
+    profile_seller = ProfileSeller.objects.get(user=request.user)
+    if request.method == 'POST':
+        part_id = request.POST.get('part')
+        car_id = request.POST.get('car') 
+        part_direction = request.POST.get('part_direction')
+        stock = request.POST.get('stock')
+        condition = request.POST.get('condition')
+        made = request.POST.get('made')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        image = request.FILES.get('logo')
+
+        product = Product(
+            part_id=part_id,
+            seller=profile_seller,
+            part_direction=part_direction,
+            made=made,
+            stock=stock,
+            condition=condition,
+            start_date=start_date,
+            end_date=end_date,
+            price=price,
+            description=description,
+            image=image
+        )
+        product.save()
+
+        if car_id:
+            car_obj = Car.objects.get(id=car_id)
+            product.car.add(car_obj)
+
     return render(request, 'sellers/seller_add_product.html', {'part_directions': Product.PartDirection.choices, "cars": cars, "part": parts })
 
 def seller_stock(request):
