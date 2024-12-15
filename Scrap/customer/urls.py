591146bb412208e670f08ee4da560b53fe2bb06d
abdullah atiq <abdullah.s.alatiq@gmail.com>
2024-12-15 23:24:03 +0300

    update profile

--- Scrap/customer/urls.py
+++ Scrap/customer/urls.py
@@ -5,6 +5,7 @@ app_name = "customer"
 
 urlpatterns = [
     path("profile_customer/", views.profile_customer, name="profile_customer"),
+    path("update_profile/", views.update_profile, name="update_profile"),
     path("cart/", views.view_cart, name="view_cart"),
     path("add_cart/<product_id>/", views.add_cart, name="add_cart"),
     path("remove_from_cart/<cart_item_id>/", views.remove_from_cart, name="remove_from_cart"),
--- Scrap/customer/views.py
+++ Scrap/customer/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import redirect,render
 from django.http import HttpRequest  
 from django.contrib import messages
 from accounts.models import ProfileCustomer  
-from .models import Cart, CartItem, Product  
+from .models import Cart, CartItem, Product
+from django.db import transaction
+from django.contrib.auth.models import User
+
 
 
 def profile_customer(request:HttpRequest):
@@ -15,6 +18,35 @@ def profile_customer(request:HttpRequest):
 
  
    return render(request, 'customer/profile_customer.html',{'profile': profile})
+
+def update_profile(request:HttpRequest):
+   if not request.user.is_authenticated:
+      messages.warning(request, "Only registered users can update their profile", "alert-warning")
+      return redirect("accounts:sign_in")
+   
+   if request.method == "POST":
+      try:
+         with transaction.atomic():
+               
+               user:User = request.user
+               user.username = request.POST["username"]
+               user.first_name = request.POST["first_name"]
+               user.last_name = request.POST["last_name"]
+               user.email = request.POST["email"]
+               user.save()
+
+               profile:ProfileCustomer = ProfileCustomer.objects.get(user=request.user)
+               profile.neighborhood = request.POST["neighborhood"]
+               profile.save()
+
+               messages.success(request, "تم تحديث الملف الشخصي بنجاح.", "alert-success")
+               return redirect("customer:profile_customer")
+      except Exception as e:
+         messages.error(request, "تعذر تحديث الملف الشخصي. حاول مرة أخرى.", "alert-danger")
+         print(f"Error during profile update: {e}")
+
+   return render(request, "customer/update_profile.html")
+
 
 
 def view_cart(request: HttpRequest):
